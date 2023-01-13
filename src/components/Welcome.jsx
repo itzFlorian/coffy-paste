30e7cada06bbd24f20fe95d26ff71d10f2eee27e
grossesbewirken <grossesbewirken@gmail.com>
2023-01-13 10:42:31 +0100

    sass aufgebaut

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,22 +1,48 @@
 import { useNavigate } from "react-router";
-import logo from "../images/coffypaste_logo_2352.png"
+
+
+// - - - - - ICONS - - - - - 
+import  bg from "/src/images/coffypaste_bg_568217968.png"
+import  avatar from "/src/images/coffypaste_icon_avatar.png"
+import  coffee from "/src/images/coffypaste_icon_coffee_default.png"
+import  community from "/src/images/coffypaste_icon_community.png"
+import  searchL from "/src/images/coffypaste_icon_search_l.png"
+import  searchS from "/src/images/coffypaste_icon_search_s.png"
+import  shop from"/src/images/coffypaste_icon_shop.png"
+import  stats from "/src/images/coffypaste_icon_stats.png"
+import  logoM from "/src/images/coffypaste_logo_900.png"
+import  logoL from "/src/images/coffypaste_logo_2352.png"
+import  efjm from "/src/images/efjm_logo.png"
+// - - - - - ICONS - - - - -
+
 
 const Welcome = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="welcome-container">
-      <div className="logo">
-        <img src="" alt="logo" />
+    <>
+      <div className="bg">
+        <img
+            src={bg}
+            className="bg-img"
+            alt="tasty coffee beans"
+        />
       </div>
-      <div className="text">
-        <p>Welcome to the tasty side</p>
+
+      <div className="welcome-container">
+        <div className="logoM-container">
+          <img src={logoM} alt="logo" />
+        </div>
+        <div className="text">
+          <h1 className="fo-sig">Welcome to the tasty side</h1>
+        </div>
+        <div className="buttons">
+          <button className="btn" onClick={()=> navigate("/registration")}>create account</button>
+          <button onClick={()=> navigate("/login")}>login</button>
+        </div>
       </div>
-      <div className="buttons">
-        <button onClick={()=> navigate("/registration")}>create account</button>
-        <button onClick={()=> navigate("/login")}>login</button>
-      </div>
-    </div>
+
+    </>
   );
 };
 
