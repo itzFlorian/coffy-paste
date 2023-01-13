033f0cabe893c9d889c5ee174d1d62bda94cbce1
grossesbewirken <grossesbewirken@gmail.com>
2023-01-13 11:41:27 +0100

    sayss extended

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -6,22 +6,12 @@ import "react-toastify/dist/ReactToastify.css"
 
 import {host} from "../api/Routes.jsx"
 
-import logo from "../images/coffypaste_logo_2352.png"
-
 
 // - - - - - ICONS - - - - - 
-import  bg from "/src/images/coffypaste_bg_568217968.png"
-import  avatar from "/src/images/coffypaste_icon_avatar.png"
-import  coffee from "/src/images/coffypaste_icon_coffee_default.png"
-import  community from "/src/images/coffypaste_icon_community.png"
-import  searchL from "/src/images/coffypaste_icon_search_l.png"
-import  searchS from "/src/images/coffypaste_icon_search_s.png"
-import  shop from"/src/images/coffypaste_icon_shop.png"
-import  stats from "/src/images/coffypaste_icon_stats.png"
-import  logoM from "/src/images/coffypaste_logo_900.png"
-import  logoL from "/src/images/coffypaste_logo_2352.png"
-import  efjm from "/src/images/efjm_logo.png"
-// - - - - - ICONS - - - - -
+import bg from "/src/images/coffypaste_bg_568217968.png"
+import logoM from "/src/images/coffypaste_logo_900.png"
+import efjm from "/src/images/efjm_logo.png"
+
 
 
 const INITIAL = {
@@ -66,6 +56,15 @@ const Registration = () => {
   }
 
   return (
+
+        <>
+      <div className="bg">
+        <img
+            src={bg}
+            className="bg-img"
+            alt="tasty coffee beans"
+        />
+      </div>
     <div className="reg-container">
       <div className="logo">
         <img src={logoM} alt="logo" />
@@ -86,7 +85,9 @@ const Registration = () => {
         <p>You already have an account? <span><NavLink to="/login">Login</NavLink></span>/<span><NavLink to="/welcome">Back</NavLink></span></p>
       </div>
       <ToastContainer/>
-    </div>
+      </div>
+      
+      </>
   );
 };
 
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,23 +2,14 @@ import { useNavigate } from "react-router";
 
 
 // - - - - - ICONS - - - - - 
-import  bg from "/src/images/coffypaste_bg_568217968.png"
-import  avatar from "/src/images/coffypaste_icon_avatar.png"
-import  coffee from "/src/images/coffypaste_icon_coffee_default.png"
-import  community from "/src/images/coffypaste_icon_community.png"
-import  searchL from "/src/images/coffypaste_icon_search_l.png"
-import  searchS from "/src/images/coffypaste_icon_search_s.png"
-import  shop from"/src/images/coffypaste_icon_shop.png"
-import  stats from "/src/images/coffypaste_icon_stats.png"
-import  logoM from "/src/images/coffypaste_logo_900.png"
-import  logoL from "/src/images/coffypaste_logo_2352.png"
-import  efjm from "/src/images/efjm_logo.png"
-// - - - - - ICONS - - - - -
+import bg from "/src/images/coffypaste_bg_568217968.png"
+import logoM from "/src/images/coffypaste_logo_900.png"
+import efjm from "/src/images/efjm_logo.png"
 
 
 const Welcome = () => {
   const navigate = useNavigate()
-console.log(logo);
+console.log(logoM);
   return (
     <>
       <div className="bg">
@@ -36,10 +27,22 @@ console.log(logo);
         <div className="text">
           <h1 className="fo-sig">Welcome to the tasty side</h1>
         </div>
-        <div className="buttons">
-          <button className="btn" onClick={()=> navigate("/registration")}>create account</button>
-          <button onClick={()=> navigate("/login")}>login</button>
+
+
+        <div className="buttons col">
+          <button
+            className="btn"
+            onClick={() => navigate("/registration")}>create account
+          </button>
+          <button
+            className="btn"
+            onClick={() => navigate("/login")}>login
+          </button>
         </div>
+      </div>
+
+      <div className="efjm-logo">
+        <img src={efjm} alt="logo of the efjm-team" />
       </div>
 
     </>
