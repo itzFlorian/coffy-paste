bb4aff2e0a8361955d99d718b169bd2111cddb86
grossesbewirken <grossesbewirken@gmail.com>
2023-01-14 12:20:26 +0100

    EFJM + Sass

---
+++ socialmedia.md
@@ -0,0 +1,43 @@
+E L E N I
+- - - -  - - - - - - - - - 
+linkedIN:
+https://www.linkedin.com/in/eleniorfanou/
+xing:
+https://www.xing.com/profile/eleni_orfanou3/cv
+github:
+https://github.com/elenosis
+
+
+
+
+F L O R I A N
+- - - -  - - - - - - - - - 
+linkedIN:
+
+xing:
+
+github:
+https://github.com/itzFlorian
+
+
+
+
+J E F F
+- - - -  - - - - - - - - - 
+linkedIN:
+https://www.linkedin.com/in/jeff-braun-0959091a4/
+xing:
+https://www.xing.com/profile/Jeff_Braun2/cv
+github:
+https://github.com/BreffJaun
+
+
+
+
+M A R T I N
+- - - -  - - - - - - - - - 
+linkedIN:
+
+xing:
+
+github:
--- src/components/EFJM.jsx
+++ src/components/EFJM.jsx
@@ -1,31 +1,31 @@
 import { useNavigate } from "react-router";
 
 
-// - - - - - ICONS - - - - - 
-import avatar from "/src/images/coffypaste_icon_avatar.png";
-import bg from "/src/images/coffypaste_bg_568217968.png";
-import logoM from "/src/images/coffypaste_logo_900.png";
-import efjm from "/src/images/efjm_logo.png";
-import xing from "./src/image/sm_xing";
-import
 
+// - - - - - ICONS - - - - - 
+import bg from "../images/coffypaste_bg_568217968.png"
+import logoM from "../images/coffypaste_logo_900.png"
+import efjm from "../images/efjm_logo.png"
+import avatar from "../images/coffypaste_icon_avatar.png"
+import xing from "../images/sm_xing.png"
+import linkedin from "../images/sm_linkedin.png"
+import github from "../images/sm_github.png"
 
 
 
 const EFJM = () => {
   const navigate = useNavigate()
-console.log(logoM);
   return (
     <>
       <div className="flex relative">
         <div className="bg"></div>
       </div>
 
-      <div className="welcome-container">
-        <div className="logoM-container">
-          <img src={logoM} alt="logo" />
-        </div>
+      <div className="logoM-container">
+        <img src={logoM} alt="logo" />
+      </div>
 
+      <div className="welcome-container">
         <div className="rotate">
           <h1 className="center">Hello, we are "efjm"</h1>
         </div>
@@ -36,17 +36,18 @@ console.log(logoM);
           </div>
           <div className="member-card bg-oldwhite">
             <h2 className="efjm-h2">Eleni</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, esse.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consectetur, esse.</p>
 
             <div className="circle-container flex row">
-              <div className="circle bg-gradL">
-
+              <div className="circle bg-gradL center">
+                <img src={linkedin} className="sm-icon"></img>
               </div>
-              <div className="circle bg-gradL">
-
+              <div className="circle bg-gradL center">
+                <img src={xing} className="sm-icon"></img>
               </div>
-              <div className="circle bg-gradL">
-
+              <div className="circle bg-gradL center">
+                <img src={github} className="sm-icon"></img>
               </div>
             </div>
             
