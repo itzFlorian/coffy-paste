1718407d2051d6f56e1e34fd68fc3cc077ec151c
BreffJaun <braun_jeff@web.de>
2023-01-16 09:51:08 +0100

    Shops/Map bearbeitet

---
+++ src/api/Google_API.jsx
@@ -0,0 +1,1 @@
+export const GOOGLE_API_KEY = "AIzaSyBufORwTJD6c4T9dszXqz6ZBvwE_O4nCbk"
--- src/components/Shops.jsx
+++ src/components/Shops.jsx
@@ -1,5 +1,5 @@
 // I M P O R T:  E X T E R N A L  D E P E N D E N C I E S
-import { GOOGLE_API_KEY } from '../api/Google_API.jsx';
+import {GOOGLE_API_KEY} from '../api/Google_API.jsx';
 import React from 'react'
 import { Map, Marker, ZoomControl, Overlay } from "pigeon-maps";
 import { host } from "../api/Routes.jsx";
@@ -31,15 +31,20 @@ const efjm = "/src/images/efjm_logo.png"
 // Get latitude & longitude from address. // Google API
 function Shops() {
 // GET USERDATA FROM USECONTEXT //
-const [shops, setShops] = useState([]);
-const [currentUser, setCurrentUser]= useContext(UserContext);
-const [user, setUser] = useState(currentUser);
+const [shops, setShops] = useState([]);         // SHOPS DATA
+const [user, setUser] = useContext(UserContext) // USER ID
+const [userData, setUserData] = useState("");   // USER DATA
+const [userLatLon, setUserLatLon] = useState({lat: 0, lon: 0})
 
+// FETCH USER LONG.- & LATITUDE //
 // Geocode.setApiKey(GOOGLE_API_KEY);
+// Geocode.setLanguage("de");
+// Geocode.setRegion("de");
 // Geocode.fromAddress(user.city).then(
 //   (response) => {
 //     const { lat, lng } = response.results[0].geometry.location;
-//     console.log(lat, lng);
+//     setUserLatLon({lat: lat, lon: lon});
+//     console(lat, lon);
 //   },
 //   (error) => {
 //     console.error(error);
@@ -47,74 +52,90 @@ const [user, setUser] = useState(currentUser);
 // );
 // console.log(Geocode);
 
+// FETCH START // 
 useEffect(() => {
-  // FETCH CURRENT USER DATA
+  // FETCH CURRENT USER ID //
+  const checkValidation = async () =>{
+    await fetch(`${host}/users/checklogin`, {
+    credentials:"include",
+    method: 'GET',   
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then(json => json.json())
+    .then(data => {    
+      // console.log("data von checkValidation", data);
+      if(data.userId){
+        setUser(data.userId)
+      }
+      if(data.error){
+        navigate("/welcome")     
+      }
+    })
+  }
+
+  // FETCH CURRENT USER DATA //
   const fetchCurrentUserData = async () =>{
-    await fetch(`${host}/users/${currentUser}`, {
+    await fetch(`${host}/users/${user}`, {
     credentials:"include",
     method: 'GET',   
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-    }
-  })
-  .then(json => json.json())
-  .then(data => setUser(data));
+      }
+    })
+    .then(json => json.json())
+    .then(data => setUserData(data));
   }
+  
+  // FETCH SHOPS DATA //
+    const fetchShops = async () =>{
+      await fetch(`${host}/coffeeshops`, {
+      credentials:"include",
+      method: 'GET',   
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then(json => json.json())
+    .then(data => setShops(data));
+  }
+
+  checkValidation()
   fetchCurrentUserData();
-  console.log(currentUser);
-  console.log(user);
-}, []);
-
-
-useEffect(()=> {
-  // FETCH SHOPS DATA START //
-  const fetchShops = async () =>{
-    await fetch(`${host}/coffeeshops`, {
-    credentials:"include",
-    method: 'GET',   
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    }
-  })
-  .then(json => json.json())
-  .then(data => setShops(data));
-  }
   fetchShops()
-},[])
+}, []);
+// FETCH END //
+console.log('user', user);
+console.log('userData', userData);
+console.log('shops', shops);
+// console.log('lat', +shops[0].location.address.latitude);
+// console.log('lon', +shops[0].location.address.longitude);
 
 
-// FETCH SHOPS DATA END //
-
-  // const [isShown, setIsShown] = useState(false);
-
-  // const shopsFiltered = allShops.filter(shop => {
-  //   return (
-  //     // Filter shops with range from longitude and latitude
-  //   )
-  // })
-
-  const overlayHandler = (e) => {
-    setIsShown(current => !current);
-  }
+const [isShown, setIsShown] = useState(false);
+const overlayHandler = (e) => {
+  setIsShown(current => !current);
+}
 
  // [latitude, longitude]
   return (
-    <>
-      <Map height={600} defaultCenter={[user.latitude, user.longitude]} defaultZoom={4}>
+    <div className='map' height={"500px"} width={"750px"}>
+      <Map height={300} width={500} defaultCenter={[51.1646177154734, 7.050247815379566]} defaultZoom={4}>
         {shops.map((shop, i) => {
           return (      
             <Marker
               width={30}
-              anchor={[shop.location.address.latitude, shop.location.address.longitude]}
+              anchor={[+shop.location.address.latitude, +shop.location.address.longitude]}
               key={i}
               onClick={(e) => overlayHandler(e)}
             />
           )
         })}
-        {(shops.map((shop, i) => {
+        {isShown && (shops.map((shop, i) => {
           return (
             <Overlay 
-              anchor={[shop.location.address.latitude, shop.location.address.longitude]} 
+              anchor={[+shop.location.address.latitude, +shop.location.address.longitude]} 
               offset={[120, 79]}>
               <div width={240} height={158} alt=''>
                 {shop.name}
@@ -124,12 +145,14 @@ useEffect(()=> {
         }))}
         <ZoomControl />
       </Map>
-      {/* <div>
+      <div>
         <ul>
-          shopsFiltered.map((shop) => li)
+          {shops.map((shop) => 
+            <li>{shop.name}</li>
+          )}
         </ul>
-      </div>  */}
-    </>
+      </div> 
+    </div>
   )
 }
 
