3888d96c5287ecb3a1d221d6e3c6bc010a1b6332
Florian <f.mewes87@gmail.com>
2023-01-16 09:51:46 +0100

    community aufgeräumt

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,7 +1,6 @@
 import Navigation from "./Navigation.jsx";
 import { useState, useEffect, useContext } from "react";
 import { host } from "../api/Routes.jsx";
-import { BsPlusCircleFill } from "react-icons/bs" 
 import { useNavigate } from "react-router-dom";
 
 import UserContext from "../context/userContext.jsx";
@@ -11,7 +10,6 @@ import UserContext from "../context/userContext.jsx";
 import searchS from "../images/coffypaste_icon_search_s.png"
 import heart from "../images/coffypaste_icon_heart.png"
 import plus from "../images/coffypaste_icon_plus.png"
-import avatar from "../images/coffypaste_icon_avatar.png"
 
 
 const Community = () => {
@@ -33,12 +31,11 @@ const Community = () => {
     })
     .then(json => {
       if(!json.ok){
-          navigate("/welcome")
-        }
-        return json.json()
+        navigate("/welcome")
+      }
+      return json.json()
     })
     .then(data => {
-      console.log(data);
       setUser(data.userId)
     })
     }
@@ -82,7 +79,7 @@ const Community = () => {
     await fetch(`${host}/users/addFriend`, {
       credentials:"include",
       method: 'POST',
-      body: JSON.stringify({friend: friend, user:user}),
+      body: JSON.stringify({friend:friend, user:user}),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       }
@@ -102,19 +99,19 @@ const Community = () => {
         </div>
       </div>
 
-      {/* ORIGINAL-CODE */}
       <div className="my-friends-container">
         <h1>my friends({currentUser?.friends && currentUser.friends.length})</h1>
-
-        {/* MUSTER-FRIEND */}
-        <div className="store-card">
+        {currentUser?.friends && currentUser.friends.map((event)=>{
+          return (
+            <>
+              <div className="store-card">
           <div className="flex center">
             <div className="iconS bg-gradL">
-              <img src={avatar} className="avatar-icon"/>
+              <img src={friend.avatar} className="avatar-icon"/>
             </div>
             <div className="col">
-              <p>friend-name</p>
-              <p>friend-coffee</p>
+              <p>{friend.userName}</p>
+              <p>{friend.favCoffee}</p>
             </div>
           </div>
           <div className=" patch-container">
@@ -125,57 +122,35 @@ const Community = () => {
             </div>
           </div>
         </div>
-        
-        {/* ORIGINAL-CODE */}
-        {currentUser?.friends && currentUser.friends.map((event)=>{
-          return (
-            <>
-              <div>
-                <img src={friend.avatar} alt="avatar" />
-                <p>{friend.userName}</p>
-                <p>{friend.favCoffee}</p>
-                <BsPlusCircleFill/>
-              </div>
             </>
           )
         })}
       </div>
 
 
-      {/* ORIGINAL-CODE */}
       <div className="all-users-container">
         <h1>all users({users && users.length})</h1>
-
-        {/* MUSTER-USER */}
-        <div className="store-card">
-          <div className="flex center">
-            <div className="iconS bg-gradD">
-              <img src={avatar} className="avatar-icon"/>
-            </div>
-            <div className="col">
-              <p>user-name</p>
-              <p>user-coffee</p>
-            </div>
-          </div>
-          <div className=" patch-container">
-            <div className="patch-btn-l row">
-              <div className="patch-btn bg-gradD center">
-                <img src={plus} className="patch-img" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* ORIGINAL-CODE */}
         <div>
           {users && users.map((user)=>{
-            return(
-            <div key={user._id}>
-              <img src={user.avatar} alt="avatar" />
-              <p>{user.userName}</p>
-              <p>{user.favCoffee}</p>
-              <BsPlusCircleFill onClick={() => addFriendHandler( user._id)}/>
-            </div>
+            return( 
+                  <div className="store-card" key={user._id}>
+                  <div className="flex center">
+                    <div className="iconS bg-gradD">
+                      <img src={user.avatar} className="avatar-icon"/>
+                    </div>
+                    <div className="col">
+                      <p>{user.userName}</p>
+                      <p>{user.favCoffee}</p>
+                    </div>
+                  </div>
+                  <div className=" patch-container">
+                    <div className="patch-btn-l row">
+                      <div className="patch-btn bg-gradD center">
+                        <img src={plus} className="patch-img" alt="" onClick={() => addFriendHandler( user._id)}/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             )
           })}          
         </div>
