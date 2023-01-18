a79a5ef966ed0f4b89f4b2ddafcbcf24c24542eb
magross <grossesbewirken@gmail.com>
2023-01-18 10:49:16 +0100

    navbarS gestylt

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,21 +3,27 @@ import { useNavigate } from "react-router";
 const Navigation = () => {
   const navigate = useNavigate()
   return (
-    <div className="flex">
-      <div className="space">
-        <button
-          className="btn"
-          onClick={() => navigate("/myaccount")}>my account</button>
-        <button
-          className="btn"
-          onClick={() => navigate("/community")}>community</button>
-        <button
-          className="btn"
-          onClick={() => navigate("/shops")}>shops</button>
-        <button
-          className="btn"
-          onClick={() => navigate("/stats")}>stats</button>
-      </div>
+    <div className="navbarS flex">
+        <div
+          className="navbar-btn"
+          onClick={() => navigate("/myaccount")}>
+            <p> my account</p>
+        </div>
+        <div
+          className="navbar-btn ml1"
+          onClick={() => navigate("/community")}>
+            <p>community</p>
+        </div>
+        <div
+          className="navbar-btn ml1"
+          onClick={() => navigate("/shops")}>
+            <p>shops</p>
+        </div>
+        <div
+          className="navbar-btn ml1"
+          onClick={() => navigate("/stats")}>
+            <p>stats</p>
+        </div>
     </div>
   );
 };
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,14 +2,13 @@
 import { useNavigate } from 'react-router'; 
 import { host } from "../api/Routes.jsx";
 
+
 // - - - - - F I L E S - - - - -
 import Navigation from "./Navigation.jsx";
 
 
 // - - - - - I M A G E S - - - - -
 import searchS from "../images/coffypaste_icon_search_s.png"
-import avatar from "../images/coffypaste_icon_avatar.png"
-import coffee from "../images/coffypaste_icon_coffee_default.png"
 import plus from "../images/coffypaste_icon_plus.png"
 
 
@@ -51,39 +50,35 @@ const Stats = () => {
       <div className="mt splitscreen">
         {/* LEFTSIDE */}
         <div>
-          <h1 className="foBE">User-Statistiken</h1>
+          <h1 className="foOW">User-Statistiken</h1>
 
-          <div className="mt2">
-            <p className="foOW">Anzahl der User:</p>
-            <h1>xyz</h1>
+          <div className="mt3">
+            <h3 className="foOW fo-le mb1">xyz</h3>
+            <p className="foOW">Anzahl der User</p>
           </div>
 
-          <div className="mt2">
-            <p className="foOW">User-Freundschaften:</p>
-            <h1>xyz</h1>
+          <div className="mt3">
+            <h3 className="foOW fo-le mb1">xyz</h3>
+            <p className="foOW">User-Freundschaften</p>
+
           </div>
         </div>
 
 
         {/* RIGHTSIDE */}
         <div>
-          <h1 className="foBE">Shop-Statistiken</h1>
+          <h1 className="foOW">Shop-Statistiken</h1>
 
-          <div className="mt2">
-            <p className="foOW">Anzahl der Shops:</p>
-            <h1>xyz</h1>
+          <div className="mt3">
+            <h3 className="foOW fo-le mb1">xyz</h3>
+            <p className="foOW">Anzahl der Shops</p>
           </div>
 
-          <div className="mt2">
-            <p className="foOW">Anzahl der Posts:</p>
-            <h1>xyz</h1>
+          <div className="mt3">
+            <h3 className="foOW fo-le mb1">xyz</h3>
+            <p className="foOW">Anzahl der Posts</p>
           </div>
-
-          <div className="mt2">
-            <p className="foOW">test</p>
-            <h1>xyz</h1>
-          </div>
-
+          
         </div>
       </div>
     </>
