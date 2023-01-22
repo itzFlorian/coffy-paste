a714e3522eb50c6a34af4a4a4021de5e415b6f65
Florian <f.mewes87@gmail.com>
2023-01-22 13:57:00 +0100

    Stats verkabelt und mit Daten befüllt

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,7 @@
 // external dependencies
 import { useNavigate } from 'react-router'; 
 import { host } from "../api/Routes.jsx";
-
+import { useEffect, useState } from 'react';
 
 // - - - - - F I L E S - - - - -
 import Navigation from "./Navigation.jsx";
@@ -14,6 +14,51 @@ import plus from "../images/coffypaste_icon_plus.png"
 
 const Stats = () => {
   const navigate = useNavigate()
+  const [shops, setShops] = useState()
+  const [users, setUsers] = useState()
+
+  // BERECHNEN DER FREUNDSCHAFTEN
+  const friendCount = users?.map(user => {
+   return user.friends.length
+  }).reduce((a,b)=> a+b, 0)
+
+  //BERECHNEN DER POSTS
+  const commentCount = shops?.map(shop => {
+    return shop.comments.length
+  }).reduce((a,b)=> a+b, 0)
+
+  useEffect(() => {
+    const fetchShops = async () =>{
+      await fetch(`${host}/coffeeshops`, {
+      credentials:"include",
+      method: 'GET',   
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then(json => json.json())
+    .then(data => setShops(data));
+  }
+
+  const fetchUsers = async () => {
+    await fetch(`${host}/users/`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((json) => json.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  };
+
+  fetchShops()
+  fetchUsers()
+  },[])
+
+
   
   // LOGOUT 
   const logout = async () => {
@@ -53,12 +98,12 @@ const Stats = () => {
           <h1 className="foOW">User-Statistiken</h1>
 
           <div className="mt3">
-            <h3 className="foOW fo-le mb1">xyz</h3>
+            <h3 className="foOW fo-le mb1">{users?.length}</h3>
             <p className="foOW">Anzahl der User</p>
           </div>
 
           <div className="mt3">
-            <h3 className="foOW fo-le mb1">xyz</h3>
+            <h3 className="foOW fo-le mb1">{friendCount}</h3>
             <p className="foOW">User-Freundschaften</p>
 
           </div>
@@ -70,12 +115,12 @@ const Stats = () => {
           <h1 className="foOW">Shop-Statistiken</h1>
 
           <div className="mt3">
-            <h3 className="foOW fo-le mb1">xyz</h3>
+            <h3 className="foOW fo-le mb1">{shops?.length}</h3>
             <p className="foOW">Anzahl der Shops</p>
           </div>
 
           <div className="mt3">
-            <h3 className="foOW fo-le mb1">xyz</h3>
+            <h3 className="foOW fo-le mb1">{commentCount}</h3>
             <p className="foOW">Anzahl der Posts</p>
           </div>
           
