f13e34550ee9aa3bc94c7a432bc0543c07dd01a8
elenosis <eleni.orfanou.88@gmail.com>
2023-01-22 14:06:55 +0100

    infotexte

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,10 @@
 import { useNavigate } from "react-router-dom";
 
-
 // images
 import avatar from "../images/coffypaste_icon_avatar.png";
 import community from "../images/coffypaste_icon_community.png";
 import shop from "../images/coffypaste_icon_shop.png";
 import stats from "../images/coffypaste_icon_stats.png";
-
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -16,6 +14,7 @@ const NavBar = () => {
         <div
           className="iconL center col"
           onClick={() => navigate("/myaccount")}
+          title="see & edit your account"
         >
           <img src={avatar} />
           <p>my account</p>
@@ -23,18 +22,27 @@ const NavBar = () => {
         <div
           className="iconL center col"
           onClick={() => navigate("/community")}
+          title="meet the coffy-paste community"
         >
           <img src={community} />
           <p>my community</p>
         </div>
       </div>
 
-      <div className="bottom row" >
-        <div className="iconL center col" onClick={() => navigate("/shops")}>
+      <div className="bottom row">
+        <div
+          className="iconL center col"
+          onClick={() => navigate("/shops")}
+          title="see the coffy-paste shops"
+        >
           <img src={shop} />
           <p>shops</p>
         </div>
-        <div className="iconL center col" onClick={() => navigate("/stats")}>
+        <div
+          className="iconL center col"
+          onClick={() => navigate("/stats")}
+          title="coffy-paste statistics"
+        >
           <img src={stats} />
           <p>stats</p>
         </div>
